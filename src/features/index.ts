2b790baf0eb54672067a11fd80421658f4d8be8f
Nekoneko <nekonekogame@gmail.com>
2021-10-04 07:51:09 +0900

    featuresの構造を修正し呼び出しを改善した

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,11 +1,15 @@
 import { App } from '../app'
 import * as fs from 'fs'
 
-export function load(app: App) {
-    const features = fs.readdirSync(__dirname).filter((file) => !file.endsWith('.js'))
+const features = {
+    init: (app: App) => {
+        const features = fs.readdirSync(__dirname).filter((file) => !file.endsWith('.js'))
 
-    for (const file of features) {
-        const feature = require(`./${file}`).default
-        feature(app)
-    }
+        for (const file of features) {
+            const feature = require(`./${file}`).default
+            feature(app)
+        }
+    },
 }
+
+export default features
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import * as dotenv from 'dotenv'
 import { App } from './app'
-import * as features from './features'
+import features from './features'
 
 // Load .env file
 dotenv.config()
 
 const app = new App(process.env.DISCORD_TOKEN, process.env.CLIENT_ID)
 
-features.load(app)
+features.init(app)
 
 process.on('SIGINT', () => {
     app.destroy()
